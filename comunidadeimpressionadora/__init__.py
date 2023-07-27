3b1df6b41d9478c3cb9ddc4bc3940ab15099396f
Valdemar Valério Roldão <116398464+Valdemarroldao@users.noreply.github.com>
2023-07-27 10:15:56 -0300

    Add files via upload

---
+++ comunidadeimpressionadora/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '4be176e438801fb9f8b7640ee27874e7exit'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.loginmessage = "Faça o Login para continuar"
+login_manager.login_message_category = 'alert-info'
+
+from comunidadeimpressionadora import routes
